4622b237c08e8811421d5e3168bb8cbf228b12e0
Gareth Tribello <gareth.tribello@gmail.com>
2021-07-15 11:53:21 +0100

    Added solution

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import numpy as np
 
-A = 
+A = np.array([[0.3,0.5,0.2],[0.3,0.4,0.3],[0.2,0.5,0.3]]) 
 
-A2 = 
-A10 = 
-A50 = 
-A100 = 
+A2 = np.linalg.matrix_power( A, 2 )
+A10 = np.linalg.matrix_power( A2, 5 )
+A50 = np.linalg.matrix_power( A10, 5 ) 
+A100 = np.linalg.matrix_power( A50,2 )
 
